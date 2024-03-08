78120339dcb348556b2e112e66ac426f2ddd30b7
Miłosz Chojecki <milosz@mioszs-macbook-air.home>
2024-03-08 23:55:04 +0100

    add hamburger menu

--- scripts.js
+++ scripts.js
@@ -17,3 +17,32 @@ var latlngs = [
 var polyline = L.polyline(latlngs, {color: 'red', weight: 5}).addTo(map);
 
 map.fitBounds(polyline.getBounds());
+
+const mapContainer = document.getElementById('map');
+
+function toggleMapVisibility() {
+  mapContainer.classList.toggle('hidden-map'); // Dodajemy lub usuwamy klasę 'hidden-map' z kontenera mapy
+}
+
+const menu = document.querySelector(".menu");
+const menuItems = document.querySelectorAll(".menuItem");
+const hamburger= document.querySelector(".hamburger");
+const closeIcon= document.querySelector(".closeIcon");
+const menuIcon = document.querySelector(".menuIcon");
+
+function toggleMenu() {
+  if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
+    toggleMapVisibility(); // Po zamknięciu menu ukrywamy mapę
+  } else {
+    menu.classList.add("showMenu");
+    closeIcon.style.display = "block";
+    menuIcon.style.display = "none";
+    toggleMapVisibility(); // Po otwarciu menu pokazujemy mapę
+  }
+}
+
+hamburger.addEventListener("click", toggleMenu);
+menuItems.forEach(menuItem => menuItem.addEventListener("click", toggleMenu));
