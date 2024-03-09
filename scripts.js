7c8f7de89273ab3921f4044579b3044b075bb87a
Miłosz Chojecki <milosz@mioszs-macbook-air.home>
2024-03-09 10:01:18 +0100

    can display path on map from uploaded file

--- scripts.js
+++ scripts.js
@@ -1,47 +1,62 @@
-//alert('Hello World!');
-
 var map = L.map('map').setView([51.11044, 17.05852], 16);
+var polyline;
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-var latlngs = [
-    [51.11057, 17.05729],
-    [51.10978, 17.05345],
-    [51.10730, 17.05663],
-    [51.10715, 17.06197]
-];
-
-var polyline = L.polyline(latlngs, {color: 'red', weight: 5}).addTo(map);
-
-map.fitBounds(polyline.getBounds());
-
 const mapContainer = document.getElementById('map');
 
 function toggleMapVisibility() {
-  mapContainer.classList.toggle('hidden-map'); // Dodajemy lub usuwamy klasę 'hidden-map' z kontenera mapy
+    mapContainer.classList.toggle('hidden-map');
 }
 
 const menu = document.querySelector(".menu");
 const menuItems = document.querySelectorAll(".menuItem");
-const hamburger= document.querySelector(".hamburger");
-const closeIcon= document.querySelector(".closeIcon");
+const hamburger = document.querySelector(".hamburger");
+const closeIcon = document.querySelector(".closeIcon");
 const menuIcon = document.querySelector(".menuIcon");
 
 function toggleMenu() {
-  if (menu.classList.contains("showMenu")) {
-    menu.classList.remove("showMenu");
-    closeIcon.style.display = "none";
-    menuIcon.style.display = "block";
-    toggleMapVisibility(); // Po zamknięciu menu ukrywamy mapę
-  } else {
-    menu.classList.add("showMenu");
-    closeIcon.style.display = "block";
-    menuIcon.style.display = "none";
-    toggleMapVisibility(); // Po otwarciu menu pokazujemy mapę
-  }
+    if (menu.classList.contains("showMenu")) {
+        menu.classList.remove("showMenu");
+        closeIcon.style.display = "none";
+        menuIcon.style.display = "block";
+        toggleMapVisibility();
+    } else {
+        menu.classList.add("showMenu");
+        closeIcon.style.display = "block";
+        menuIcon.style.display = "none";
+        toggleMapVisibility();
+    }
 }
 
 hamburger.addEventListener("click", toggleMenu);
+
+document.getElementById('fileInput').addEventListener('change', function(event) {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = function(e) {
+        const gpxData = e.target.result;
+        const gpx = new DOMParser().parseFromString(gpxData, 'text/xml');
+        const trackPoints = gpx.querySelectorAll('trkpt');
+        const latlngs = [];
+
+        trackPoints.forEach(function(point) {
+            const lat = parseFloat(point.getAttribute('lat'));
+            const lon = parseFloat(point.getAttribute('lon'));
+            latlngs.push([lat, lon]);
+        });
+
+        if (polyline) {
+            map.removeLayer(polyline);
+        }
+
+        polyline = L.polyline(latlngs, {color: 'blue'}).addTo(map);
+        map.fitBounds(polyline.getBounds());
+    };
+
+    reader.readAsText(file);
+});
